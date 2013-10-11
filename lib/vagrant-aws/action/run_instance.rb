c0a6fb99a849b66057cef82d7d9af7648ab2b4d7
TANABE Ken-ichi <nabeken@tknetworks.org>
2013-10-11 11:20:21 +0900

    spot: Waiting for spot request ready

--- lib/vagrant-aws/action/run_instance.rb
+++ lib/vagrant-aws/action/run_instance.rb
@@ -219,6 +219,10 @@ module VagrantPlugins
             break if env[:interrupted]
             spot_req = env[:aws_compute].describe_spot_instance_requests(
               'spot-instance-request-id' => [spot_request_id]).body["spotInstanceRequestSet"].first
+
+            # waiting for spot request ready
+            next unless spot_req
+
             # display something whenever the status code changes
             if status_code != spot_req["fault"]["code"]
               env[:ui].info("Status: #{spot_req["fault"]["message"]}")
